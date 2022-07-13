f4174efbfa008a53613746ed522771456ec1e09b
sunjo <sunjo@prismweaver.com>
2022-07-13 14:32:34 +0900

    3.2 Testing getAll and getOne

---
+++ src/movies/movies.service.spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
+
+describe('MoviesService', () => {
+  let service: MoviesService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [MoviesService],
+    }).compile();
+
+    service = module.get<MoviesService>(MoviesService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+});
